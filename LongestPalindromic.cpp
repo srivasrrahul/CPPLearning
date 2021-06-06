39aaea5b5c93a3e0e5c02b1a943a116d88e6832a
Srivastava <srivasrrahul@gmal.com>
2021-06-06 18:49:11 +0530

    More problems C++

--- LongestPalindromic.cpp
+++ LongestPalindromic.cpp
@@ -5,74 +5,101 @@
 #include <vector>
 #include <unordered_map>
 #include <set>
+class Decision {
+private:
+    int m_Low;
+    int m_High;
+public:
+    Decision(int mLow, int mHigh) : m_Low(mLow), m_High(mHigh) {}
+
+    int getMLow() const {
+        return m_Low;
+    }
+
+    int getMHigh() const {
+        return m_High;
+    }
+
+    bool operator==(const Decision &rhs) const {
+        return m_Low == rhs.m_Low &&
+               m_High == rhs.m_High;
+    }
+
+    bool operator!=(const Decision &rhs) const {
+        return !(rhs == *this);
+    }
+
+
+};
+
+struct Hash {
+    size_t operator()(const Decision & d) const {
+        return d.getMLow()+d.getMHigh();
+    }
+};
 using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-
-        unordered_map<char,vector<int>> charIndexes;
-
+        vector<vector<int>> dp;
         for (int j = 0;j<s.length();++j) {
-            charIndexes.emplace(s[j],vector<int>{});
-            auto & indexes = charIndexes[s[j]];
-            indexes.push_back(j);
-        }
-
-
-
-        //cout << charIndexes.size() << endl;
-
-        std::function<bool(int,int)> checkPalindrome = [&](int low,int high) {
-            if (low == high) {
-                return true;
-            }else {
-                if (low == high-1) {
-                    return s[low] == s[high];
+            dp.push_back(vector<int>{});
+            for (int k = 0;k<s.length();++k) {
+                if (j == k) {
+                    dp[j].push_back(1);
                 }else {
-                    if (s[low] == s[high]) {
-                        return checkPalindrome(low+1,high-1);
-                    }else {
-                        return false;
-                    }
+                    dp[j].push_back(0);
                 }
             }
-        };
+        }
+
+        auto colStarter = 1;
+        int j = 0;
+        int k = colStarter;
 
         auto maxLen = 1;
-        auto low = 0;
-        auto high = 0;
-        for (int j = 0;j<s.length();++j) {
-            auto largestPalindromLen = 1;
-            auto highLocal = s.length()-1;
-            auto found = false;
-            //cout << s[j] << ", "<<charIndexes[s[j]].size() << endl;
-//             for (auto x : charIndexes[s[j]]) {
-//                 cout << x << ",";
-//             }
-//             cout << endl;
-
-//             cout << endl;
-            for (vector<int>::reverse_iterator it = charIndexes[s[j]].rbegin(); *it>j && (*it-j+1)>largestPalindromLen;++it) {
-                //cout << "For " << j << "," << *it << endl;
-                if (checkPalindrome(j,*it)) {
-                    //cout << "Palindome " << j << "," << *it << endl;
-                    largestPalindromLen = (*it-j+1);
-                    highLocal = *it;
-                    found = true;
-                    break;
+        while (j < s.length() && k < s.length()) {
+            if (j+1 == k) {
+                if (s[j] == s[k]) {
+                    dp[j][k] = 2;;
+                }else {
+                    dp[j][k] = 0;
                 }
-
+            }else {
+                if (dp[j+1][k-1] > 0) {
+                    if (s[j] == s[k]) {
+                        dp[j][k] = dp[j+1][k-1]+2;
+                    }else {
+                        dp[j][k] = 0;
+                    }
+                }else {
+                    dp[j][k] = 0;
+                }
             }
 
-            if (found && largestPalindromLen > maxLen) {
-                //cout << "Found " << j << "," << highLocal << endl;
-                low = j;
-                high = highLocal;
-                maxLen = largestPalindromLen;
+            maxLen = max(maxLen,dp[j][k]);
+
+            j = j+1;
+            k = k+1;
+            if (k >= s.length()) {
+                colStarter++;
             }
 
+            if (j >= s.length() || k >= s.length()) {
+                j = 0;
+                k = colStarter;
+            }
         }
 
-        return s.substr(low,high-low+1);
+        for (int j = 0;j<s.length();++j) {
+            for (int k = j;k<s.length();++k) {
+                if (dp[j][k] == maxLen) {
+                    return s.substr(j,maxLen);
+                }
+            }
+        }
+
+        return 0;
+
     }
 };
